3c9d2c6ea896316060aa6c23d550bcf87dcf9233
Todd Hendricks <hendricks.ta@gmail.com>
2021-03-08 14:55:48 -0800

    authenticate s3 client using the os module and calling environment variables

--- kinesis_test.py
+++ kinesis_test.py
@@ -1,2 +1,9 @@
 import boto3
-print("hello world")
+import os
+s3 = boto3.client(
+    's3',                   
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),                   
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),                   
+    region_name='us-west-2'
+)
+print(s3)
