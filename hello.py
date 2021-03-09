ed15fea6ad7ec35d1e44dfe1814f6f14a80785f1
Todd Hendricks <hendricks.ta@gmail.com>
2021-03-09 15:27:27 -0800

    include form html in templates folder

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
